f2c7b07545de09793a684e2b4f6209351230e43c
Ricardo Silvestre <ricardomsilvestre@outlook.com>
2021-01-07 19:13:25 -0300

    primeiro

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Difficulty, fetchQuizQuestions, QuestionState } from './API';
+import QuestionCard from './components/QuestionCard';
 
-function App() {
+type AnswerObject = {
+  question: string;
+  answer: string;
+  correct: boolean;
+  correctAnswer: string;
+}
+
+const TOTAL_QUESTIONS = 10;
+
+const App = () => {
+  const [loading, setLoading] = React.useState(false);
+  const [questions, setQuestions] = React.useState<QuestionState[]>([]);
+  const [number, setNumber] = React.useState(0);
+  const [userAnswers, setUserAnswers] = React.useState<AnswerObject[]>([]);
+  const [score, setScore] = React.useState(0);
+  const [gameOver, setGameOver] = React.useState(true);
+
+  fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty)
+
+  const startTrivia = async () => {
+
+  }
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+
+  }
+
+  const nextQuestion = () => {
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Quiz</h1>
+      <button className="start" onClick={startTrivia}>
+        Start
+      </button>
+      <p className="score">Score: </p>
+      <p>Loading Questions...</p>
+      <QuestionCard questionNr={number + 1} totalQuestions={TOTAL_QUESTIONS} question={questions[number].question} answers={questions[number].answers} userAnswer={userAnswers ? userAnswers[number] : undefined} callback={checkAnswer} />
+      <button className="next" onClick={nextQuestion}>Next Question</button>
     </div>
   );
 }
