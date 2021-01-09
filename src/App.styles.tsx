f10e2394d9be5c788b04b61d627ba9574e64f9b4
Ricardo Silvestre <ricardomsilvestre@outlook.com>
2021-01-09 15:45:04 -0300

    finalizado

--- src/App.styles.tsx
+++ src/App.styles.tsx
@@ -37,7 +37,7 @@ export const Wrapper = styled.div`
 
     h1 {
         font-family: Fascinate Inline, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
-        background: linear-gradient(180deg, #fff, #87f1ff);
+        background: linear-gradient(180deg, #fff, #fb1);
         background-size: 100%;
         background-clip: text;
         -webkit-background-clip: text;
@@ -47,5 +47,22 @@ export const Wrapper = styled.div`
         filter: drop-shadow(2px 2px #0085a3);
         font-size: 70px;
         text-align: center;
+        font-weight: 400;
         margin: 20px;
-    }`
+    }
+    
+    .start, .next {
+        cursor: pointer;
+        background: linear-gradient(180deg, #fff, #ffcc91);
+        border: 2px solid #d38558;
+        box-shadow: 0px 5px 10px rgba(0, 0, 0, .25);
+        border-radius: 10px;
+        height: 40px;
+        margin: 20px 0;
+        padding: 0 40px;
+    }
+
+    .start {
+        max-width: 200px;
+    }
+`
